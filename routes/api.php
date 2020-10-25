5dda94ddd589c76ecc69bf473d2c3763394e29fa
michaelhu <hbj148@gmail.com>
2020-10-25 21:47:43 +0800

    change api

--- routes/api.php
+++ routes/api.php
@@ -48,17 +48,5 @@ Route::group(
         'domain' => $_domain_tenant,
         'middleware' => ['checkHeader', 'checkLandlord', 'auth:api', 'checkUser']
     ], function () use ($_methodGet, $_methodPost, $_methodPut, $_methodDelete) {
-
-    Route::match($_methodPost, 'comment/create', [CommentAPIController::class, 'apiCreate'])->name('comment.write.create');
-    Route::match($_methodGet, 'comment/read/item', [CommentAPIController::class, 'apiReadItem'])->name('comment.read.item');
-    Route::match($_methodGet, 'comment/read/list', [CommentAPIController::class, 'apiReadList'])->name('comment.read.list');
-    Route::match($_methodPut, 'comment/update', [CommentAPIController::class, 'apiUpdate'])->name('comment.write.update');
-    Route::match($_methodDelete, 'comment/delete', [CommentAPIController::class, 'apiDelete'])->name('comment.write.Delete');
-
-    Route::match($_methodPost, 'todo/create', [ToDoAPIController::class, 'apiCreate'])->name('todo.write.create');
-    Route::match($_methodGet, 'todo/read/item', [ToDoAPIController::class, 'apiReadItem'])->name('todo.read.item');
-    Route::match($_methodGet, 'todo/read/list', [ToDoAPIController::class, 'apiReadList'])->name('todo.read.list');
-    Route::match($_methodPut, 'todo/update', [ToDoAPIController::class, 'apiUpdate'])->name('todo.write.update');
-    Route::match($_methodDelete, 'todo/delete', [ToDoAPIController::class, 'apiDelete'])->name('todo.write.Delete');
-
+        
 });
