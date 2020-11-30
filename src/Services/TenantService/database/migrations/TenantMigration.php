806057d348487ada142b51272bf0f49676067fcf
michaelhu <hbj148@gmail.com>
2020-11-30 23:33:36 +0800

    Tenant migration connetion need to be solved couple 耦合配置

--- src/Services/TenantService/database/migrations/TenantMigration.php
+++ src/Services/TenantService/database/migrations/TenantMigration.php
@@ -16,6 +16,11 @@ class TenantMigration extends Migration
 
     function __construct()
     {
+        /**
+         * 这里有两个场景：
+         * 一是，是在初始化的时候，使用root tenant来初始化项目
+         * 二是，如果是每次给用户开tenant时，需要用"tenant" connection来migrate用户的基础表
+         */
         $this->schema = Schema::connection(TenantModel::getConnectionNameStatic())->getConnection()->getSchemaBuilder();
 //        dd($this->schema);
 
